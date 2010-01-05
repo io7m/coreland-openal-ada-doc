3038b1a6925dd58a2bdd85994f5a107d789683fc
User <m0@viper.internal.network>
2010-01-05 04:00:39 +0000

    Initial version

---
+++ local.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+OPENAL_SRC="${HOME}/git/coreland/openal-ada/"
+
+fatal()
+{
+  echo "fatal: $1" 1>&2
+  exit 1
+}
+
+info()
+{
+  echo "info: $1" 1>&2
+}
+
+#
+# Make procedure map
+#
+
+info "building procedure map"
+
+cat >> src/m_proc_map.ud.tmp <<EOF
+(table procedure_map
+  (t-row (item "OpenAL C API") (item "Ada equivalent"))
+EOF
+
+(
+for spec in ${OPENAL_SRC}/openal-*.ads
+do
+  info "processing $spec"
+  ./make-proc_map.lua "$spec" || fatal "could not create proc map for $spec"
+done
+) | sort -k 3 >> src/m_proc_map.ud.tmp
+
+echo ")" >> src/m_proc_map.ud.tmp
+
+mv src/m_proc_map.ud.tmp src/m_proc_map.ud || fatal "could not replace m_proc_map.ud"
